5ef4c9e1c10d22c25edcc673db7fb66817df91fc
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2021-11-03 00:36:55 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/khanifzyen/pml_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khanifzyen.pml_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'app/utils/error_page.dart';
+import 'app/routes/app_pages.dart';
+import 'app/utils/loading_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return ErrorPage();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Chat App",
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }
+
+        return LoadingPage();
+      },
+    );
+  }
+}
