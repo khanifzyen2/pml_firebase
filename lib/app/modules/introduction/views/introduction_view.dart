c6210c64f07c0ca6b22445e7e8f416961aa85705
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2021-11-03 23:56:56 +0700

    introduction screen done

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -1,21 +1,76 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pml_firebase/app/routes/app_pages.dart';
 
 import '../controllers/introduction_controller.dart';
 
 class IntroductionView extends GetView<IntroductionController> {
+  List<PageViewModel> listPagesViewModel = [
+    PageViewModel(
+      title: "Berinteraksi dengan mudah",
+      body: "Kamu hanya perlu dirumah saja untuk mendapatkan teman baru",
+      image: Container(
+        width: Get.width * 0.6,
+        height: Get.width * 0.6,
+        child: Center(
+          child: Lottie.asset("assets/lottie/main-laptop-duduk.json"),
+        ),
+      ),
+    ),
+    PageViewModel(
+      title: "Temukan sahabat baru",
+      body: "Jika kamu memang jodoh karena aplikasi ini, kami sangat bahagia",
+      image: Container(
+        width: Get.width * 0.6,
+        height: Get.width * 0.6,
+        child: Center(
+          child: Lottie.asset("assets/lottie/ojek.json"),
+        ),
+      ),
+    ),
+    PageViewModel(
+      title: "Aplikasi bebas biaya",
+      body: "Kamu tidak perlu khawatir, aplikasi ini bebas biaya apapun",
+      image: Container(
+        width: Get.width * 0.6,
+        height: Get.width * 0.6,
+        child: Center(
+          child: Lottie.asset("assets/lottie/payment.json"),
+        ),
+      ),
+    ),
+    PageViewModel(
+      title: "Gabung sekarang juga",
+      body:
+          "Daftarkan diri kamu untuk menjadi bagian dari kami. Kami akan menghubungkan dengan 1000 teman lainnya",
+      image: Container(
+        width: Get.width * 0.6,
+        height: Get.width * 0.6,
+        child: Center(
+          child: Lottie.asset("assets/lottie/register.json"),
+        ),
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('IntroductionView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'IntroductionView is working',
-          style: TextStyle(fontSize: 20),
+      body: IntroductionScreen(
+        pages: listPagesViewModel,
+        onDone: () => Get.offAllNamed(Routes.LOGIN),
+        showSkipButton: true,
+        skip: Text("Skip"),
+        next: Text(
+          "Next",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        done: const Text(
+          "Login",
+          style: TextStyle(fontWeight: FontWeight.w600),
         ),
       ),
     );
