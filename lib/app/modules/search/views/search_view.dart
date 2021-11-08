0d55fa36a33799062167fd7b5b0d4581d63b7100
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2021-11-08 22:58:38 +0700

    create search screen done

--- lib/app/modules/search/views/search_view.dart
+++ lib/app/modules/search/views/search_view.dart
@@ -1,23 +1,97 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pml_firebase/app/routes/app_pages.dart';
 
 import '../controllers/search_controller.dart';
 
 class SearchView extends GetView<SearchController> {
+  final List<Widget> friends = List.generate(
+    20,
+    (index) => ListTile(
+      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+      leading: CircleAvatar(
+        radius: 30,
+        backgroundColor: Colors.black26,
+        child: Image.asset(
+          "assets/logo/noimage.png",
+          fit: BoxFit.cover,
+        ),
+      ),
+      title: Text(
+        "Orang ke ${index + 1}",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+      subtitle: Text(
+        "orang${index + 1}@gmail.com",
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+      ),
+      trailing: Chip(
+        label: Text("3"),
+      ),
+    ),
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('SearchView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'SearchView is working',
-          style: TextStyle(fontSize: 20),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(140),
+        child: AppBar(
+          title: Text('Search'),
+          centerTitle: true,
+          backgroundColor: Colors.red[900],
+          leading: IconButton(
+            onPressed: () => Get.back(),
+            icon: Icon(Icons.arrow_back),
+          ),
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 50, 30, 20),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: TextField(
+                cursorColor: Colors.red[900],
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.white, width: 1),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.white, width: 1),
+                  ),
+                  hintText: "Search New Friend",
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  suffixIcon: InkWell(
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.red[900],
+                    ),
+                    onTap: () {},
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
+      body: friends.length == 0
+          ? Center(
+              child: Container(
+                width: Get.width * 0.7,
+                height: Get.width * 0.7,
+                child: Lottie.asset("assets/lottie/empty.json"),
+              ),
+            )
+          : ListView.builder(
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) => friends[index],
+              itemCount: friends.length,
+            ),
     );
   }
 }
