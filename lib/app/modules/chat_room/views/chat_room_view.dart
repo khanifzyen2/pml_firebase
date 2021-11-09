10daf13d9357ad2e48d327dfa0f1b4f60ff48756
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2021-11-09 18:41:27 +0700

    add textfield, emoji iconbutton  and send button

--- lib/app/modules/chat_room/views/chat_room_view.dart
+++ lib/app/modules/chat_room/views/chat_room_view.dart
@@ -9,14 +9,83 @@ class ChatRoomView extends GetView<ChatRoomController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ChatRoomView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ChatRoomView is working',
-          style: TextStyle(fontSize: 20),
+        backgroundColor: Colors.red[900],
+        leadingWidth: 100,
+        leading: InkWell(
+          onTap: () {},
+          borderRadius: BorderRadius.circular(100),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(width: 5),
+              Icon(Icons.arrow_back),
+              SizedBox(width: 5),
+              CircleAvatar(
+                radius: 25,
+                backgroundColor: Colors.grey,
+                child: Image.asset("assets/logo/noimage.png"),
+              ),
+            ],
+          ),
         ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Lorem Ipsum",
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              "statusnya lorem",
+              style: TextStyle(fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: Get.width,
+              height: 100,
+              color: Colors.green,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            width: Get.width,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //TextField(),
+                Expanded(
+                  child: Container(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.emoji_emotions_outlined),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100)),
+                      ),
+                    ),
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.red[900],
+                  child: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
