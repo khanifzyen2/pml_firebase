b6e6fc02896600ce5df54ef0ba8d754e4332b80a
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2021-11-11 14:51:54 +0700

    firebase auth done

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pml_firebase/app/routes/app_pages.dart';
@@ -6,25 +7,44 @@ class AuthController extends GetxController {
   var isSkipIntro = false.obs;
   var isAuth = false.obs;
 
-  GoogleSignIn _googleSignIn = GoogleSignIn();
+  final _googleSignIn = GoogleSignIn();
   GoogleSignInAccount? _currentUser;
 
   Future<void> login() async {
     //Buat fungsi untuk login dengan google
     try {
+      //ini untuk handle kebocoran data user sebelum login
       await _googleSignIn.signOut();
-      await _googleSignIn.signIn().then((value) => _currentUser = value);
-      await _googleSignIn.isSignedIn().then((value) {
-        if (value) {
-          //kondisi login berhasil
-          print("SUDAH BERHASIL LOGIN DENGAN AKUN : ");
-          print(_currentUser);
-          isAuth.value = true;
-          Get.offAllNamed(Routes.HOME);
-        } else {
-          print("TIDAK BERHASIL LOGIN");
-        }
-      });
+
+      //ini digunakan untuk mendapatkan google account
+      _currentUser = await _googleSignIn.signIn();
+
+      //ini untuk mengecek status login user
+      final isSignedIn = await _googleSignIn.isSignedIn();
+
+      if (isSignedIn) {
+        //kondisi login berhasil
+        print("SUDAH BERHASIL LOGIN DENGAN AKUN : ");
+        print(_currentUser);
+
+        final googleAuth = await _currentUser!.authentication;
+
+        final _credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        );
+
+        final userCredential =
+            await FirebaseAuth.instance.signInWithCredential(_credential);
+
+        print("USER CREDENTIALS");
+        print(userCredential);
+
+        isAuth.value = true;
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        print("TIDAK BERHASIL LOGIN");
+      }
     } catch (error) {
       print(error);
     }
